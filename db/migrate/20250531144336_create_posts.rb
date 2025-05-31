658a52d7eb0687d5dedf033991ecb303ca132b61
Rene Hernandez <rnhe88@gmail.com>
2025-05-31 08:48:52 -0600

    first commit

---
+++ db/migrate/20250531144336_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[8.0]
+  def change
+    create_table :posts do |t|
+      t.string :titulo
+      t.text :pregunta
+      t.text :respuesta
+
+      t.timestamps
+    end
+  end
+end
